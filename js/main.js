d12c0e09d823c28e54f63cc563e3d8dd4ba909b1
Ariel Cotton <ariel@electricstud.io>
2013-12-12 02:06:00 -0500

    working on responsiveness, mobile navigation, and header

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/* This function is for the mobile navigation expansion.
+ * When you click on the expansion button, it toggles the mobile
+ * navigation by alternately adding and removing an .active class.
+ */
+function expandMobileNav() {
+	
+	var navExpansion = $("#nav-expansion");
+	var navigation = $("#navigation-mobile");
+	var header = $("#header");
+
+	$(navExpansion).click(function(event) {
+		
+		$(this).toggleClass("active");
+
+		if ($(this).hasClass('active')) {
+			$(navigation).show();
+			$(header).animate({
+        		height: '+=50px'
+    		}, 500);
+		}
+
+		if (!$(this).hasClass('active')) {
+			$(navigation).hide();
+			$(header).animate({
+        		height: '-=50px'
+    		}, 500);
+		}
+
+	});
+
+}
+
+$(window).resize(function() {
+
+	var navigation = $("#navigation-mobile");
+	var navExpansion = $("#nav-expansion");
+	var header = $("#header");
+
+	if (($(window).width() > 760) && $(navExpansion).hasClass("active")) {
+        $(navigation).hide();
+		$(navExpansion).removeClass("active");
+		$(header).animate({
+        	height: '-=50px'
+    	}, 500);
+    }
+
+});
+
+$(document).ready(function() {
+ 
+    expandMobileNav();
+ 
+});
