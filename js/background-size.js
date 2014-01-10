a04290a4bca1b9b98a9838b0debcd3f2d6f61993
amsheehan <alex.m.sheehan@gmail.com>
2014-01-10 10:14:32 -0500

    expand out to desktop

--- js/background-size.js
+++ js/background-size.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     var windowHeight = $(document).height()
+    var topHatHeight = $('.header-section').height()    
+    var sectionOneHeight = windowHeight - topHatHeight
 
     $('.section').css('height',windowHeight+'px')
+    $('.section1').css('height', sectionOneHeight + "px")
 
 });
--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-
-    $('.mobile-tray').click(function() {
-        var trayPos = $('.mobile-nav').css('left','0')
-    });
-
-    $('.collapse-nav').click(function() {
-        var trayPos = $('.mobile-nav').css('left','-100%')
+    var trayOut = false;
+    
+    $('.tray-button').click(function() {
+        if(!trayOut){
+            var contentPos = $('#all-content').css('left','85%')
+            
+            document.ontouchmove = function(e){
+                e.preventDefault();
+            }
+        } else {
+            var contentPos = $('#all-content').css('left','0')
+        
+            document.ontouchmove = function(e){
+                return true;
+            }
+        }
+        trayOut = !trayOut
     });
 
 });
