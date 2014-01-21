75eaf5c1dda6603ff2aa8b1925010306976d819a
amsheehan <alex.m.sheehan@gmail.com>
2014-01-20 20:26:17 -0500

    cross fade icons and lists

--- js/perks.js
+++ js/perks.js
@@ -1,7 +1,49 @@
-function revealList(obj) {
-    obj.style.opacity = 0;
-}
+$(document).ready(function() {
+    $('.perks-icon1').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy1').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy1').css('opacity','0');
+    });
 
-function hideList(obj) {
-    obj.style.opacity = 1;
-}
+    $('.perks-icon2').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy2').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy2').css('opacity','1');
+    });
+
+    $('.perks-icon3').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy3').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy3').css('opacity','1');
+    });
+
+    $('.perks-icon4').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy4').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy4').css('opacity','1');
+    });
+
+    $('.perks-icon5').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy5').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy5').css('opacity','1');
+    });
+  
+    $('.perks-icon6').mouseover(function(){
+        $(this).css('opacity','0');
+        $('#perks-copy6').css('opacity','1');
+    }).mouseout(function() {
+        $(this).css('opacity','1');
+        $('#perks-copy6').css('opacity','1');
+    });
+})
