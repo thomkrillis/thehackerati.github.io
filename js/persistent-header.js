52975563157db0c28b7f81f3f2d364a6eb09c7df
Ariel Cotton <ariel@electricstud.io>
2014-02-13 11:24:42 -0500

    making more changes to persistent header

--- js/persistent-header.js
+++ js/persistent-header.js
@@ -1,9 +1,8 @@
 window.onscroll = function (oEvent) {
   var header = document.getElementById("section1-anchor-link").offsetTop;
-  var indexHeader = document.getElementById("welcome-container").offsetTop;
   var scrollPos = document.getElementsByTagName("body")[0].scrollTop;
 
-  if(scrollPos >= header+100 || scrollPos >= indexHeader+2000){
+  if(scrollPos >= header+100){
     document.getElementById("persistent-header").className = "visible";
     console.log("just scrolled down");
   }
