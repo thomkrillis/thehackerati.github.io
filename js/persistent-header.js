1a9dcffd1134e17c6bfe13cc972d393d2943cbb4
Eddy <Eddy@thehackerati.com>
2014-02-19 21:55:12 -0500

    fixed header fix for safari and safari mobile

--- js/persistent-header.js
+++ js/persistent-header.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-  if(/Android|webOS|iPhone|iPod|iPad|BlackBerry/i.test(navigator.userAgent) ) {}
+  if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {}
   else {
-    window.onscroll = function (oEvent) {
-      var scrollPos = document.documentElement.scrollTop;
-      var offsetHeight = document.getElementById('welcome-container').offsetHeight;
+    window.onscroll = function () {
+      var scrollPos = window.pageYOffset;
+      var offsetHeight = document.getElementById('top-section').clientHeight;
 
-      if(scrollPos >= offsetHeight){
-        document.getElementById('sb-nav').style.opacity = "1";
+      if(scrollPos >= offsetHeight || window.innerWidth < 768){
+        document.getElementById('sb-nav-home').style.display = "block";
+        document.getElementById('sb-nav-home').style.opacity = "1";
       }
       else {
-        document.getElementById('sb-nav').style.opacity = "0";
+        document.getElementById('sb-nav-home').style.opacity = "0";
+        document.getElementById('sb-nav-home').style.display = "none";
       }
     }
   }
