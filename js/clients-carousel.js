3f1f9ebd8990ed45cc1d92146dc78df0c371a414
Eddy <Eddy@thehackerati.com>
2014-02-20 14:21:40 -0500

    client carousel fix for android tablets

--- js/clients-carousel.js
+++ js/clients-carousel.js
@@ -1,5 +1,5 @@
 $(window).on('resize', function() {
-  if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || window.innerWidth < 768 ) {
+  if(/webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || window.innerWidth < 768 || (/Android/i.test(navigator.userAgent) && window.innerWidth < 768 )) {
     var clientsOpen = false; 
     var tileFlags = new Array();
       
