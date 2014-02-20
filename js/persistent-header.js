6022b3146de5b1d88eecfe318785f72f34f06f2a
Eddy <Eddy@thehackerati.com>
2014-02-20 14:06:58 -0500

    fixed header not showing up on IOS devices

--- js/persistent-header.js
+++ js/persistent-header.js
@@ -7,10 +7,8 @@ $(document).ready(function() {
 
       if(scrollPos >= offsetHeight || window.innerWidth < 768){
         document.getElementById('sb-nav-home').style.display = "block";
-        document.getElementById('sb-nav-home').style.opacity = "1";
       }
       else {
-        document.getElementById('sb-nav-home').style.opacity = "0";
         document.getElementById('sb-nav-home').style.display = "none";
       }
     }
