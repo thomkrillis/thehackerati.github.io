b64f9d229ad27a9634ec3694f9f359ebd4f409fe
amsheehan <alex.m.sheehan@gmail.com>
2014-03-29 11:15:40 -0400

    alexBlogLogo sorry no ticket. added an icon or something

--- js/expand.js
+++ js/expand.js
@@ -1,13 +1,22 @@
-var expanded = false,
-    numOfListItems = $('.post-list').children().length,
-    newHeight = 22 * numOfListItems;
+$(document).ready(function () {
 
-$('.expand-archive').click(function () {
-    if (!expanded) {
-        $('.terminal-body').css('height', newHeight + 'px');
-        expanded = !expanded;
-    } else {
-        $('.terminal-body').css('height','254px');
-        expanded = !expanded;
-    }
+  var expanded = false,
+    numOfListItems = $('.post-list').children().length,
+    newHeight = 24 * numOfListItems,
+    consoleHeight = $('terminal-body').innerHeight();
+
+  if (newHeight >= 253) {
+    $('.expand-archive').click(function () {
+        if (!expanded) {
+            $('.terminal-body').css('height', newHeight + 'px');
+            expanded = !expanded;
+        } else {
+            $('.terminal-body').css('height','254px');
+            expanded = !expanded;
+        }   
+      });
+  } else {
+    $('.expand-archive').remove();
+    $('.terminal-body').css('height', newHeight + 'px');
+  }
 });
