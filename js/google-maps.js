ee89d09c3859e1ef3bc9dfc5dd06d357747ff6b2
Bobby Yankou <thisisjackolantern@gmail.com>
2014-09-30 15:02:57 -0400

    Revert "HACK-587: smooth header transition on home page"
    This reverts commit 1ccaac61ecd3afe3b33e45a58a3ab19fac73f267.

--- js/google-maps.js
+++ js/google-maps.js
@@ -154,19 +154,13 @@ function slideMaps() {
 
 function scrollHandler(){
   var scrollPos = window.pageYOffset;
-  var offsetHeight = document.getElementById('welcome-container').clientHeight - 65;
+  var offsetHeight = document.getElementById('welcome-container').clientHeight;
 
   if(scrollPos >= offsetHeight || window.innerWidth < 768){
-    document.getElementById('content-container').style.marginTop = "0";
-    document.getElementById('nav-links').style.lineHeight = "55px";
-    document.getElementById('sb-nav-home').style.height = "65px";
-    document.getElementById('content-container').style.height = "55px";
+    document.getElementById('sb-nav-home').style.display = "block";
   }
   else {
-    document.getElementById('sb-nav-home').style.height = "180px";
-    document.getElementById('content-container').style.marginTop = "2.5em";
-    document.getElementById('nav-links').style.lineHeight = "130px";
-    document.getElementById('content-container').style.height = "130px";
+    document.getElementById('sb-nav-home').style.display = "none";
   }
 
   if (!isMapDisplayed && isElementInViewport(document.getElementById('contact-title'))) {
