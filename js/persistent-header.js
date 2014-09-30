3bc4a45e32354185d751814077c299da9e47efe1
Bobby Yankou <thisisjackolantern@gmail.com>
2014-09-30 15:16:49 -0400

    Revert "Revert "HACK-587: make transition header for b-pages as well""
    This reverts commit f5060b63164140cfc306a1dea714d3300f6aea59.

--- js/persistent-header.js
+++ js/persistent-header.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
   else {
     window.onscroll = function () {
       var scrollPos = window.pageYOffset;
-      var offsetHeight = document.getElementById('top-section').clientHeight;
+      var offsetHeight = document.getElementById('top-section').clientHeight - 85;
 
       if(scrollPos >= offsetHeight || window.innerWidth < 768){
-        document.getElementById('sb-nav-home').style.display = "block";
+        document.getElementById('nav-links').style.lineHeight = "55px";
+        document.getElementById('sb-nav-home').style.height = "65px";
+        document.getElementById('content-container').style.marginTop = "0";
+        document.getElementById('content-container').style.height = "55px";
       }
       else {
-        document.getElementById('sb-nav-home').style.display = "none";
+        document.getElementById('sb-nav-home').style.height = "98px";
+        document.getElementById('nav-links').style.lineHeight = "68px";
+        document.getElementById('content-container').style.marginTop = "10px";
+        document.getElementById('content-container').style.height = "68px";
       }
     }
   }
@@ -17,22 +23,34 @@ $(document).ready(function() {
 
 $(window).on("touchmove", function() {
       var scrollPos = window.pageYOffset;
-      var offsetHeight = document.getElementById('top-section').clientHeight;
+      var offsetHeight = document.getElementById('top-section').clientHeight - 85;
 
       if(scrollPos >= offsetHeight || window.innerWidth < 768){
-        document.getElementById('sb-nav-home').style.display = "block";
+        document.getElementById('nav-links').style.lineHeight = "55px";
+        document.getElementById('sb-nav-home').style.height = "65px";
+        document.getElementById('content-container').style.marginTop = "0";
+        document.getElementById('content-container').style.height = "55px";
       }
       else {
-        document.getElementById('sb-nav-home').style.display = "none";
+        document.getElementById('sb-nav-home').style.height = "98px";
+        document.getElementById('nav-links').style.lineHeight = "68px";
+        document.getElementById('content-container').style.marginTop = "10px";
+        document.getElementById('content-container').style.height = "68px";
       }
 })
 
 $(window).on('resize', function() {
-    if (window.innerWidth < 768){
-      document.getElementById('sb-nav-home').style.display = "block";
+    if(scrollPos >= offsetHeight || window.innerWidth < 768){
+      document.getElementById('nav-links').style.lineHeight = "55px";
+      document.getElementById('sb-nav-home').style.height = "65px";
+      document.getElementById('content-container').style.marginTop = "0";
+      document.getElementById('content-container').style.height = "55px";
     }
-    else{
-      document.getElementById('sb-nav-home').style.display = "none";
+    else {
+      document.getElementById('sb-nav-home').style.height = "98px";
+      document.getElementById('nav-links').style.lineHeight = "68px";
+      document.getElementById('content-container').style.marginTop = "10px";
+      document.getElementById('content-container').style.height = "68px";
     }
 });
 
